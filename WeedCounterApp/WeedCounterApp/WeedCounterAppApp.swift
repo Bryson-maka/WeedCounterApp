9c290ae5ae90cfaf6a49396fb85a88e4bc5735af
Bryson Corey <bryson@maka-ars.com>
2024-09-23 20:04:06 -0700

    Initial project setup

---
+++ WeedCounterApp/WeedCounterApp/WeedCounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeedCounterAppApp.swift
+//  WeedCounterApp
+//
+//  Created by Bryson Corey on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct WeedCounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeedCounterApp/WeedCounterAppTests/WeedCounterAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeedCounterAppTests.swift
+//  WeedCounterAppTests
+//
+//  Created by Bryson Corey on 9/23/24.
+//
+
+import Testing
+
+struct WeedCounterAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
